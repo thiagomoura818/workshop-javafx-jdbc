7cb1364f5a89a32556137aa03bb2c8a33280b560
thiagomoura818 <waatsaaynew@gmail.com>
2021-07-26 10:51:49 -0300

    DepartmentList Controller

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -61,6 +61,7 @@ public class MainViewController implements Initializable{
 			
 		}catch(IOException e) {
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+			System.out.println(e.getMessage());
 		}
 		
 	}
